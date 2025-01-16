5e714e9151ca610c39a68347baf2b9c2f19c2e31
vik_vas <vasilevskiviktor@yahoo.com>
2025-01-16 19:24:26 +0100

    create db migration

---
+++ Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+            
+    }
+}
